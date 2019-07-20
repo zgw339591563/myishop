485e35e0dc5dbd288900af25d600b0c1ad188b69
339591563@qq.com <339591563@qq.com>
2019-07-21 02:05:11 +0800

    init

--- meite-shop-service-api/meite-shop-service-api-weixin/src/main/java/com/mayikt/weixin/service/VerificaCodeService.java
+++ meite-shop-service-api/meite-shop-service-api-weixin/src/main/java/com/mayikt/weixin/service/VerificaCodeService.java
@@ -1,16 +1,10 @@
 package com.mayikt.weixin.service;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alibaba.fastjson.JSONObject;
 import com.mayikt.base.BaseResponse;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 
@@ -23,7 +17,7 @@ import io.swagger.annotations.ApiOperation;
  * @Copyright 该项目“基于SpringCloud2.x构建微服务电商项目”由每特教育|蚂蚁课堂版权所有，未经过允许的情况下，
  *            私自分享视频和源码属于违法行为。
  */
-@Api(tags = "微信注册码验证码接口")
+//@Api(tags = "微信注册码验证码接口")
 public interface VerificaCodeService {
 
 	/**
@@ -31,12 +25,12 @@ public interface VerificaCodeService {
 	 * 
 	 * @return
 	 */
-	@ApiOperation(value = "根据手机号码验证码token是否正确")
+//	@ApiOperation(value = "根据手机号码验证码token是否正确")
 	@PostMapping("/verificaWeixinCode")
-	@ApiImplicitParams({
-			// @ApiImplicitParam(paramType="header",name="name",dataType="String",required=true,value="用户的姓名",defaultValue="zhaojigang"),
-			@ApiImplicitParam(paramType = "query", name = "phone", dataType = "String", required = true, value = "用户手机号码"),
-			@ApiImplicitParam(paramType = "query", name = "weixinCode", dataType = "String", required = true, value = "微信注册码") })
+//	@ApiImplicitParams({
+//			// @ApiImplicitParam(paramType="header",name="name",dataType="String",required=true,value="用户的姓名",defaultValue="zhaojigang"),
+//			@ApiImplicitParam(paramType = "query", name = "phone", dataType = "String", required = true, value = "用户手机号码"),
+//			@ApiImplicitParam(paramType = "query", name = "weixinCode", dataType = "String", required = true, value = "微信注册码") })
 	public BaseResponse<JSONObject> verificaWeixinCode(@RequestParam("phone") String phone,
-			@RequestParam("weixinCode") String weixinCode);
+													   @RequestParam("weixinCode") String weixinCode);
 }
