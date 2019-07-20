789e683be98a7a1b3c2bfb9ffebd425bf3c573af
339591563@qq.com <339591563@qq.com>
2019-07-20 23:57:56 +0800

    保持sql

--- meite-shop-service-impl/meite-shop-service-spike/src/main/java/com/mayikt/spike/service/impl/OrderSeckillServiceImpl.java
+++ meite-shop-service-impl/meite-shop-service-spike/src/main/java/com/mayikt/spike/service/impl/OrderSeckillServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mayikt.spike.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
