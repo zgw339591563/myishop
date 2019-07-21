8a0ba14a6282da6c9529560ce39952daf72ef566
339591563@qq.com <339591563@qq.com>
2019-07-21 23:23:05 +0800

    放开建造者模式

--- meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/zuul/filter/GatewayFilter.java
+++ meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/zuul/filter/GatewayFilter.java
@@ -17,6 +17,7 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
+import com.mayikt.zuul.build.GatewayDirector;
 
 /**
  * 使用网关拦截客户端所有的请求
@@ -37,6 +38,8 @@ public class GatewayFilter extends ZuulFilter {
 	private BlacklistMapper blacklistMapper;
 	@Autowired
 	private ResponsibilityClient responsibilityClient;
+	@Autowired
+	private GatewayDirector gatewayDirector;
 
 	/**
 	 * 请求之前拦截处理业务逻辑 建议将限制黑名单存放到redis或者携程的阿波罗
@@ -47,8 +50,11 @@ public class GatewayFilter extends ZuulFilter {
 		// 1.获取请求对象
 		HttpServletRequest request = ctx.getRequest();
 		HttpServletResponse response = ctx.getResponse();
+		//基于责任链模式实现网关权限控制
 		GatewayHandler handler = responsibilityClient.getHandler();
 		handler.service(ctx, request, response);
+		//基于建造者模式实现网关权限控制---和责任链模式2选一
+//		gatewayDirector.direcot(ctx,getIpAddr( request),response,request);
 		return null;
 	}
 	// public/api/api-pay/cratePayToken?payAmount=300222&orderId=2019010203501502&userId=644064
@@ -110,4 +116,31 @@ public class GatewayFilter extends ZuulFilter {
 
 	// 签名的目的是 为了防止数据被篡改 数据还是明文数据
 	// 加密 RSA
+
+
+	/**
+	 * 获取Ip地址
+	 *
+	 * @param request
+	 * @return
+	 */
+	public String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("X-Forwarded-For");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
 }
