7e3ede4b7f97ae129782065657f331c6faa09cf8
339591563@qq.com <339591563@qq.com>
2019-08-09 23:10:51 +0800

    去除无用工具

--- meite-shop-service-impl/meite-shop-service-weixin/src/main/java/com/mayikt/weixin/service/impl/VerificaCodeServiceImpl.java
+++ meite-shop-service-impl/meite-shop-service-weixin/src/main/java/com/mayikt/weixin/service/impl/VerificaCodeServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ctc.wstx.util.StringUtil;
 import com.mayikt.base.BaseApiService;
 import com.mayikt.base.BaseResponse;
 import com.mayikt.constants.Constants;
